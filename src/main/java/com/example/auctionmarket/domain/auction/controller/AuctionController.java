f1e23d96ca9319c02893b307ee2e85168f9d10d1
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-04-07 11:55:39 +0900

    feat : 경매 상태값 변경 기능 추가

--- src/main/java/com/example/auctionmarket/domain/auction/controller/AuctionController.java
+++ src/main/java/com/example/auctionmarket/domain/auction/controller/AuctionController.java
@@ -58,7 +58,7 @@ public class AuctionController {
     ){
 
         Page<AuctionResponse> result = auctionService.SearchAuctions(
-                keyword, category, page, size
+                keyword, category, /*authUser,*/ page, size
         );
 
         return ResponseEntity.ok(result);
