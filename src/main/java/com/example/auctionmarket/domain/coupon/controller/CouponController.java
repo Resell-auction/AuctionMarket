a0ca7b83a9b1359ca915632dbd4bb903a8b4b88e
bopeep934 <gkinfn@gmail.com>
2025-04-07 10:23:45 +0900

    feat(coupon): coupon기능추가

--- src/main/java/com/example/auctionmarket/domain/coupon/controller/CouponController.java
+++ src/main/java/com/example/auctionmarket/domain/coupon/controller/CouponController.java
@@ -1,5 +1,6 @@
 package com.example.auctionmarket.domain.coupon.controller;
 
+import com.example.auctionmarket.domain.coupon.dto.CouponGiveRequest;
 import com.example.auctionmarket.domain.coupon.dto.CouponRequest;
 import com.example.auctionmarket.domain.coupon.dto.CouponResponse;
 import com.example.auctionmarket.domain.coupon.dto.CouponUpdateRequest;
@@ -16,12 +17,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/v1/coupons")
 public class CouponController {
+
     private final CouponService couponService;
 
     //등록
     @PostMapping//admin만 가능
-    public ResponseEntity<CouponResponse> createCoupon(@RequestBody CouponRequest couponRequest){
-        return ResponseEntity.ok(couponService.createCoupon(couponRequest));
+    public ResponseEntity<CouponResponse> createCoupon(@AuthenticationPrincipal AuthUser authUser, @RequestBody CouponRequest couponRequest){
+        return ResponseEntity.ok(couponService.createCoupon(authUser, couponRequest));
     }
 
     //전체 목록 조회
@@ -38,13 +40,19 @@ public class CouponController {
 
     //수정
     @PutMapping("/{id}")//admin
-    public ResponseEntity<CouponResponse> updateCoupon(@PathVariable Long id, @RequestBody CouponUpdateRequest couponUpdateRequest){
+    public ResponseEntity<CouponResponse> updateCoupon(@AuthenticationPrincipal AuthUser authUser,@PathVariable Long id, @RequestBody CouponUpdateRequest couponUpdateRequest){
         return ResponseEntity.ok(couponService.updateById(id, couponUpdateRequest));
     }
 
     //삭제
     @DeleteMapping("/{id}")//admin
-    public void deleteCoupon(@PathVariable Long id){
+    public void deleteCoupon(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long id){
         couponService.deleteById(id);
+    }
+
+    //유저에게 쿠폰을 원하는 수량만큼 주기
+    @PostMapping("/{id}")
+    public void giveCouponByUserId(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long id, @RequestBody CouponGiveRequest couponGiveRequest){
+        giveCouponByUserId(id, couponGiveRequest);
     }
 }
--- src/main/java/com/example/auctionmarket/domain/coupon/service/CouponService.java
+++ src/main/java/com/example/auctionmarket/domain/coupon/service/CouponService.java
@@ -1,9 +1,11 @@
 package com.example.auctionmarket.domain.coupon.service;
 
+import com.example.auctionmarket.domain.coupon.dto.CouponGiveRequest;
 import com.example.auctionmarket.domain.coupon.dto.CouponRequest;
 import com.example.auctionmarket.domain.coupon.dto.CouponResponse;
 import com.example.auctionmarket.domain.coupon.dto.CouponUpdateRequest;
 import com.example.auctionmarket.domain.coupon.entity.Coupon;
+import com.example.auctionmarket.domain.coupon.enums.CouponStatus;
 import com.example.auctionmarket.domain.coupon.repository.CouponRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +21,13 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
 
+    //admin - 쿠폰생성
     @Transactional
-    public CouponResponse createCoupon(CouponRequest couponRequest) {
-        //나중에 AuthUser 추가 admin 권한 검사.
+    public CouponResponse createCoupon(AuthUser authUser, CouponRequest couponRequest) {
+        if(authUser.getUserRole()!=UserStatus.ADMIN){
+            throw new AccessDeniedException("관리자만 접근할 수 있습니다.");
+        };
+
         Coupon coupon = new Coupon(couponRequest.getCouponName(),
                 couponRequest.getDescription(),
                 couponRequest.getDiscountRate(),
@@ -38,9 +44,10 @@ public class CouponService {
                 savedCoupon.getAmount());
     }
 
+    //쿠폰목록조회
     @Transactional(readOnly = true)
     public List<CouponResponse> findAll() {
-        List<Coupon> coupons = couponRepository.findAll();
+        List<Coupon> coupons = couponRepository.findByCouponStatus(CouponStatus.VALID);//사용가능한 쿠폰만 조회
         List<CouponResponse> couponList = new ArrayList<>();
 
         for (Coupon coupon : coupons)
@@ -54,8 +61,13 @@ public class CouponService {
         return couponList;
     }
 
+    //쿠폰단건조회
     @Transactional(readOnly = true)
-    public CouponResponse findById(Long id) {
+    public CouponResponse findById(AuthUser authUser, Long id) {
+        if(authUser.getUserRole()!=UserStatus.ADMIN){
+            throw new AccessDeniedException("관리자만 접근할 수 있습니다.");
+        };
+
         Coupon coupon = couponRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException(" 찾는 쿠폰이 없습니다."));
 
@@ -67,8 +79,13 @@ public class CouponService {
                 coupon.getAmount());
     }
 
+    //admin- 쿠폰수정
     @Transactional
-    public CouponResponse updateById(Long id, CouponUpdateRequest couponUpdateRequest) {
+    public CouponResponse updateById(AuthUser authUser, Long id, CouponUpdateRequest couponUpdateRequest) {
+        if(authUser.getUserRole()!=UserStatus.ADMIN){
+            throw new AccessDeniedException("관리자만 접근할 수 있습니다.");
+        };
+
         Coupon coupon = couponRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException(" 찾는 쿠폰이 없습니다."));
 
@@ -85,11 +102,32 @@ public class CouponService {
                 coupon.getAmount());
     }
 
+    //admin- 쿠폰삭제
     @Transactional
-    public void deleteById(Long id) {//enum으로 수정
+    public void deleteById(AuthUser authUser, Long id) {
+        if(authUser.getUserRole()!=UserStatus.ADMIN){
+            throw new AccessDeniedException("관리자만 접근할 수 있습니다.");
+        };
+
         Coupon coupon = couponRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException(" 찾는 쿠폰이 없습니다."));
 
-        couponRepository.deleteById(id);
+        coupon.expiredCoupon();
+    }
+
+    //admin- 해당유저에게 쿠폰주기
+    @Transactional
+    public void giveCouponByUserId(Long id, CouponGiveRequest couponGiveRequest){
+        Coupon coupon = couponRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException(" 찾는 쿠폰이 없습니다."));
+
+        User user = userRepository.findById(couponGiveRequest.getUserId()).orElseThrow(
+                () -> new IllegalArgumentException(" 찾는 유저가 없습니다. ");
+        )//user는 쿠폰 리스트를 갖고 있음.
+
+        user.setCoupon(couponGiveRequest.getAmount());
+
+        coupon.discountCoupon(couponGiveRequest.getAmount());
+
     }
 }
