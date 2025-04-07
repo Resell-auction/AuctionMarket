34491f7e54fc2a53e18387c51b74a8dc73096cab
Yujun <pathfinder357@github.com>
2025-04-07 11:12:26 +0900

    redis 관련 삭제(2)

--- src/main/java/com/example/auctionmarket/domain/auth/exception/AuthenticationExpiredException.java
+++
@@ -1,9 +0,0 @@
-package com.example.auctionmarket.domain.auth.exception;
-
-import jakarta.security.auth.message.AuthException;
-
-public class AuthenticationExpiredException extends AuthException {
-	public AuthenticationExpiredException() {
-		super(AuthErrorCode.AUTHENTICATION_EXPIRED);
-	}
-}
