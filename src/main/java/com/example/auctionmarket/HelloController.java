275af42229205a70efe1ae6fad5aec78a4c916ef
uyr83157 <uyr83157@gmail.com>
2025-04-08 10:19:24 +0900

    test: cicd 테스트 - 배포 작업에 빌드된 이미지 url 전달 성공 - EC2 연결 확인을 위한 헬로 월드 테스트

---
+++ src/main/java/com/example/auctionmarket/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.auctionmarket;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+
+	@GetMapping("/2")
+	public String hello2() {
+		return "Hello World2";
+	}
+
+}
