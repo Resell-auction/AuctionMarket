43f2be309d2df6f7f54b2314119f941529e24e2a
Yujun <pathfinder357@github.com>
2025-04-09 19:44:27 +0900

    testcode progressing

---
+++ src/test/java/com/example/auctionmarket/domain/users/Service/ServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.auctionmarket.domain.users.Service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ServiceTest {
+}
