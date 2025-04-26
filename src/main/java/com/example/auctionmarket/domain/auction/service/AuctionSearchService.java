99a8ad636f3bc322c5d2122ad1b72f7855309df1
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-04-26 23:49:55 +0900

    feat : opensearch ec2

--- src/main/java/com/example/auctionmarket/domain/auction/service/AuctionSearchService.java
+++ src/main/java/com/example/auctionmarket/domain/auction/service/AuctionSearchService.java
@@ -1,7 +1,7 @@
 package com.example.auctionmarket.domain.auction.service;
 
 import com.example.auctionmarket.domain.auction.document.AuctionDocument;
-import com.example.auctionmarket.domain.auction.repository.AuctionSearchRepository;
+//import com.example.auctionmarket.domain.auction.repository.AuctionSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -11,24 +11,24 @@ import org.springframework.util.StringUtils;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@Service
-public class AuctionSearchService {
-
-    private final AuctionSearchRepository auctionSearchRepository;
-
-    public Page<AuctionDocument> searchAuctions(String keyword, String category, Pageable pageable) {
-        Page<AuctionDocument> result = auctionSearchRepository.findByProductNameContainingIgnoreCase(keyword, pageable);
-
-        //카테고리 필터
-        if(StringUtils.hasText(category)) {
-            List<AuctionDocument> filtered = result.getContent().stream()
-                    .filter(doc ->doc.getCategory().equalsIgnoreCase(category))
-                    .toList();
-
-            return new PageImpl<>(filtered, pageable, filtered.size());
-        }
-
-        return result;
-    }
-}
+//@RequiredArgsConstructor
+//@Service
+//public class AuctionSearchService {
+//
+//    private final AuctionSearchRepository auctionSearchRepository;
+//
+//    public Page<AuctionDocument> searchAuctions(String keyword, String category, Pageable pageable) {
+//        Page<AuctionDocument> result = auctionSearchRepository.findByProductNameContainingIgnoreCase(keyword, pageable);
+//
+//        //카테고리 필터
+//        if(StringUtils.hasText(category)) {
+//            List<AuctionDocument> filtered = result.getContent().stream()
+//                    .filter(doc ->doc.getCategory().equalsIgnoreCase(category))
+//                    .toList();
+//
+//            return new PageImpl<>(filtered, pageable, filtered.size());
+//        }
+//
+//        return result;
+//    }
+//}
