a922573a1aba52cafc918607d58f810342397f3e
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-05-04 17:11:24 +0900

    feat : auction testcode

--- src/test/java/com/example/auctionmarket/domain/auction/service/AuctionServiceTest.java
+++ src/test/java/com/example/auctionmarket/domain/auction/service/AuctionServiceTest.java
@@ -10,6 +10,8 @@ import com.example.auctionmarket.domain.auction.dto.response.AuctionPageResponse
 import com.example.auctionmarket.domain.auction.dto.response.AuctionResponse;
 import com.example.auctionmarket.domain.auction.dto.response.AuctionSaveResponse;
 import com.example.auctionmarket.domain.auction.entity.Auction;
+import com.example.auctionmarket.domain.auction.enums.AuctionStatus;
+import com.example.auctionmarket.domain.auction.exception.AuctionException;
 import com.example.auctionmarket.domain.auction.repository.AuctionRepository;
 import com.example.auctionmarket.domain.payment.service.PaymentService;
 import com.example.auctionmarket.domain.product.entity.Product;
@@ -17,11 +19,13 @@ import com.example.auctionmarket.domain.product.enums.ProductCategory;
 import com.example.auctionmarket.domain.product.repository.ProductRepository;
 import com.example.auctionmarket.domain.user.entity.User;
 import com.example.auctionmarket.domain.user.enums.Role;
+import com.example.auctionmarket.domain.user.exception.UserNotFoundException;
 import com.example.auctionmarket.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -175,6 +179,27 @@ public class AuctionServiceTest {
     }
 
     @Test
+    public void 경매_참여_유저_없음(){
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(UserNotFoundException.class, () -> auctionService.join(authUser, 1L));
+    }
+
+    @Test
+    public void 경매_참여_경매_없음(){
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.join(authUser, 1L);
+        });
+    }
+
+    @Test
     public void 경매_최소_가격_수정_성공(){
         // given
         given(userRepository.findById(1L)).willReturn(Optional.of(user));
@@ -186,6 +211,78 @@ public class AuctionServiceTest {
 
         // then
         assertEquals(20000L, auction.getMinPrice());
+    }
+
+    @Test
+    public void 경메_최소_가격_수정_유저_없음() {
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(UserNotFoundException.class, () -> {
+            auctionService.updateMinPrice(authUser, 1L, new AuctionUpdateMinPriceRequest(20000L));
+        });
+    }
+
+    @Test
+    public void 경매_최소_가격_수정_경매_없음() {
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateMinPrice(authUser, 1L, new AuctionUpdateMinPriceRequest(20000L));
+        });
+    }
+
+    @Test
+    public void 경매_최소_가격_수정_작성자_아님() {
+        // given
+        User otherUser = new User("other@email.com", "pass1234!", "other", "010-1234-5678", Role.USER);
+        setIdUsingReflection(otherUser, 2L);
+
+        Product otherProduct = new Product(otherUser, "otherProduct", "content", ProductCategory.CLOTHES);
+        setIdUsingReflection(otherProduct, 2L);
+
+        Auction otherAuction = new Auction(otherProduct, 10000L, LocalDateTime.now(), 1L);
+        setIdUsingReflection(otherAuction, 2L);
+
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(2L)).willReturn(Optional.of(otherAuction));
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateMinPrice(authUser, 2L, new AuctionUpdateMinPriceRequest(20000L));
+        });
+    }
+
+    @Test
+    public void 경매_최소_가격_수정_진행_중_예외() {
+        // given
+        auction.updateStatus(AuctionStatus.ONGOING);
+
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.of(auction));
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateMinPrice(authUser, 1L, new AuctionUpdateMinPriceRequest(20000L));
+        });
+    }
+
+    @Test
+    public void 경매_최소_가격_수정_종료됨_예외() {
+        // given
+        auction.updateStatus(AuctionStatus.ENDED);
+
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.of(auction));
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateMinPrice(authUser, 1L, new AuctionUpdateMinPriceRequest(20000L));
+        });
     }
 
     @Test
@@ -203,4 +300,165 @@ public class AuctionServiceTest {
         assertNotNull(response);
         assertEquals(request.getUpdateTime(), auction.getStartTime());
     }
+
+    @Test
+    public void 경매_시작_시간_수정_유저_없음(){
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(UserNotFoundException.class, () -> {
+            auctionService.updateAuctionStartTime(authUser, 1L, new AuctionUpdateTimeRequest(LocalDateTime.now()));
+        });
+    }
+
+    @Test
+    public void 경매_시작_시간_수정_경매_없음(){
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateAuctionStartTime(authUser, 1L, new AuctionUpdateTimeRequest(LocalDateTime.now()));
+        });
+    }
+
+    @Test
+    public void 경매_시작_시간_수정_작성자_아님(){
+        // given
+        User otherUser = new User("other@email.com", "pass1234!", "other", "010-1111-2222", Role.USER);
+        setIdUsingReflection(otherUser, 2L);
+
+        Product otherProduct = new Product(otherUser, "otherProduct", "content", ProductCategory.CLOTHES);
+        setIdUsingReflection(otherProduct, 2L);
+
+        Auction otherAuction = new Auction(otherProduct, 10000L, LocalDateTime.now(), 2L);
+        setIdUsingReflection(otherAuction, 2L);
+
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(2L)).willReturn(Optional.of(otherAuction));
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateAuctionStartTime(authUser, 2L, new AuctionUpdateTimeRequest(LocalDateTime.now()));
+        });
+    }
+
+    @Test
+    public void 경매_시작_시간_수정_진행_중_예외() {
+        // given
+        auction.updateStatus(AuctionStatus.ONGOING);
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.of(auction));
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateAuctionStartTime(authUser, 1L, new AuctionUpdateTimeRequest(LocalDateTime.now()));
+        });
+    }
+
+    @Test
+    public void 경매_시작_시간_수정_종료됨_예외() {
+        // given
+        auction.updateStatus(AuctionStatus.ENDED);
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.of(auction));
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.updateAuctionStartTime(authUser, 1L, new AuctionUpdateTimeRequest(LocalDateTime.now()));
+        });
+    }
+
+    @Test
+    public void 경매_삭제_성공(){
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.of(auction));
+
+        // when
+        auctionService.deleteAuction(authUser, 1L);
+
+        // then
+        verify(auctionRepository, times(1)).delete(auction);
+    }
+
+    @Test
+    public void 경매_삭제_유저_없음_예외(){
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(UserNotFoundException.class, () -> {
+            auctionService.deleteAuction(authUser, 1L);
+        });
+    }
+
+    @Test
+    public void 경매_삭제_경매_없음(){
+        // given
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.deleteAuction(authUser, 1L);
+        });
+    }
+
+    @Test
+    public void 경매_삭제_작성자_아님(){
+        // given
+        User otherUser = new User("other@email.com", "pass1234!", "other", "010-1111-2222", Role.USER);
+        setIdUsingReflection(otherUser, 2L);
+        Product otherProduct = new Product(otherUser, "otherProduct", "content", ProductCategory.CLOTHES);
+        setIdUsingReflection(otherProduct, 2L);
+        Auction otherAuction = new Auction(otherProduct, 10000L, LocalDateTime.now().plusHours(1), 2L);
+        setIdUsingReflection(otherAuction, 2L);
+
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(auctionRepository.findById(2L)).willReturn(Optional.of(otherAuction));
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.deleteAuction(authUser, 2L);
+        });
+    }
+
+    @Test
+    public void 경매_종료_성공_consumerId_없음(){
+        // given
+        Auction endedAuction = new Auction(product, 10000L, LocalDateTime.now().minusHours(3), 1L);
+        setIdUsingReflection(endedAuction, 1L);
+
+        given(auctionRepository.findById(1L)).willReturn(Optional.of(endedAuction));
+
+        // when
+        auctionService.endAuction(1L);
+
+        System.out.println("현재 시간: " + LocalDateTime.now());
+        System.out.println("종료 시간: " + endedAuction.getEndTime());
+        System.out.println("isAfter(): " + LocalDateTime.now().isAfter(endedAuction.getEndTime()));
+
+        // then
+        ArgumentCaptor<Auction> captor = ArgumentCaptor.forClass(Auction.class);
+        verify(auctionRepository).save(captor.capture());
+
+        Auction saved = captor.getValue();
+
+        assertEquals(AuctionStatus.ENDED, saved.getStatus());
+    }
+
+    @Test
+    public void 경매_종료_예외_경매없음() {
+        // given
+        given(auctionRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThrows(AuctionException.class, () -> {
+            auctionService.endAuction(1L);
+        });
+    }
+
 }
